6128191a06e3e6ec3447f6a74ea4c0f8261cc7fc
sanatan223 <sanatan_dev+github@outlook.com>
2025-03-25 21:25:00 +0530

    codded basic algorithm

--- server.js
+++ server.js
@@ -3,9 +3,22 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
+app.get('/:slug', (req, res) => {
+    if (req.params.slug == "about"){
+        res.send('this is about page')
+    }
+    else if (req.params.slug == "contact-me"){
+        res.send("this is my contact page")
+    }
+    else {
+        res.send('oops page not found')
+    }
+})
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })

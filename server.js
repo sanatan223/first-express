f95ed2f10d048a6ada5f7ee683d653487954317b
sanatan223 <sanatan_dev+github@outlook.com>
2025-03-26 20:24:56 +0530

    simplified the code and uninstalled some modules

--- server.js
+++ server.js
@@ -5,19 +5,15 @@ const port = 3000
 app.get('/', (req, res) => {
     res.sendFile('pages/index.html', { root: __dirname })
 })
-
-app.get('/:slug', (req, res) => {
-    if (req.params.slug == "about"){
-        res.sendFile('pages/about.html', { root: __dirname })
-    }
-    else if (req.params.slug == "contact-me"){
-        res.sendFile('pages/contacts.html', { root: __dirname })
-    }
-    else {
-        res.sendFile('pages/404.html', { root: __dirname })
-    }
+app.get('/about', (req, res) => {
+    res.sendFile('pages/about.html', { root: __dirname })
 })
-
+app.get('/contact-me', (req, res) => {
+    res.sendFile('pages/contacts.html', { root: __dirname })
+})
+app.get('/*', (req, res) => {
+    res.sendFile('pages/404.html', { root: __dirname })
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
